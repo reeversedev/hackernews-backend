06e01c7388f2ff5324e2cc627663219c03d8a723
Prateek Gogia <gogia.prateek@hotmail.com>
2018-09-23 18:01:43 +0530

    Created a simple query

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 const { GraphQLServer } = require("graphql-yoga");
 
 //1
-const typeDefs = `type Query {
+const typeDefs = `
+type Query {
     info: String!
-}`;
+    feed: [Link]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}
+`;
 
 //2
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
+//3
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: root => root.id,
+    description: root => root.description,
+    url: root => root.url
   }
 };
 
-//3
 const server = new GraphQLServer({ typeDefs, resolvers });
 
 server.start(() => console.log("Server is running on http://localhost:4000"));
