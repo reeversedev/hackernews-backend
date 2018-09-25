76916ea945bb9de2d9a6bd350cfb5086b71d88bd
Prateek Gogia <gogia.prateek@hotmail.com>
2018-09-25 08:49:44 +0530

    Setup express server

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-//1
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
-
-//2
 let links = [
   {
     id: "link-0",
@@ -23,19 +8,28 @@ let links = [
   }
 ];
 
-//3
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
   },
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers
+});
 
 server.start(() => console.log("Server is running on http://localhost:4000"));
